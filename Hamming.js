7511c564eca324a9f6408954bea85e359fe71eeb
jchaphiv <jchaphiv@gmail.com>
2016-03-29 12:14:25 -0400

    Create Hamming.js

---
+++ Hamming.js
@@ -0,0 +1,17 @@
+var compute;
+compute = function(x,y){
+	var count = 0;
+
+	if (x.length !== y.length) throw new Error('DNA strands must be of equal length.');
+
+	for (var i = x.length - 1; i >= 0; i-- ){
+
+		if (x.charAt(i) !== y.charAt(i)) count ++;
+	};
+
+	return count;
+	};
+
+module.exports = {
+	compute: compute,
+};
